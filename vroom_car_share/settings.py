96a739931c0d61e9b79cde0e9898e9bf6ad90214
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-12 15:43:21 +1000

    Change language to English AU, time zone to Melbourne

--- vroom_car_share/settings.py
+++ vroom_car_share/settings.py
@@ -103,8 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-au'
+TIME_ZONE = 'Australia/Melbourne'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
