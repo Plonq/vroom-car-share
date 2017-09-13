df1e85b027cefe5dc35ad3a149a51163f69e10c2
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-13 16:38:35 +1000

    Enable editing of user profile in django admin

--- carshare/admin.py
+++ carshare/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from carshare.models import UserProfile
+
+
+# Define an inline admin descriptor for UserProfile model
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        """Prevents profile fields when creating new user"""
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
