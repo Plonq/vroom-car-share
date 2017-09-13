2959ab90800cdf2d2f637dda697563442953ba2c
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-13 16:05:35 +1000

    Rename Driver -> UserProfile
    This matches convention and probably makes more sense.
    Also a UserProfile object is automatically created when a
    User is created, however profile fields can be blank.

--- carshare/models.py
+++ carshare/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 import datetime
 
 
-class Driver(models.Model):
+class UserProfile(models.Model):
     """
-    Stores extra information about users that are also drivers (customers)
+    Stores user profile information
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def is_over_18(self):
         """Returns True if date of birth is at least 18 years in the past to the day"""
         return self.date_of_birth <= datetime.date.today() - datetime.timedelta(days=18*365)
+
+
+# Signals to create/save UserProfile object when User is created/saved
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
