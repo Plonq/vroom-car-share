a073202a52f8f93e27bb5dd6752f8eb038e1bb6a
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-13 13:43:28 +1000

    Add Driver model
    Driver has a OneToOne relationship with User, effectively
    extending it to add extra data.

--- carshare/models.py
+++ carshare/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+
+class Driver(models.Model):
+    """
+    Stores extra information about users that are also drivers (customers)
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+
+    def is_over_18(self):
+        """Returns True if date of birth is at least 18 years in the past to the day"""
+        return self.date_of_birth <= datetime.date.today() - datetime.timedelta(days=18*365)
