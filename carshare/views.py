f37c7919d38e6d613aba46fe83627d4481cca177
connii <connie9991@hotmail.com>
2017-09-14 10:29:25 +1000

    Addition of styling for about page

--- carshare/views.py
+++ carshare/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'carshare/index.html')
+
+
+def about(request):
+    return render(request, 'carshare/about.html')
