4da48cd72f905f4a83545c6ba09eccca8cea48d0
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-19 09:08:02 +1000

    Move master templates to more standard location

--- vroom_car_share/settings.py
+++ vroom_car_share/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'vroom_car_share.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'vroom_car_share/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
