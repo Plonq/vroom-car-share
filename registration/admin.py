b5e022d1d8f7e3e7b06b37348cf177da2895525f
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-19 11:43:33 +1000

    Configure django-admin with appropriate staff permissions
    Users that have is_staff=True are not allowed to modify their own
    permissions. Similarly, they can not edit any superusers, or users
    with custom permissions. This should prevent staff members increasing
    their own privileges (e.g. making themselves a superuser).

--- registration/admin.py
+++ registration/admin.py
@@ -32,10 +32,26 @@ class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline, AddressInline, CreditCardInline)
 
     def get_inline_instances(self, request, obj=None):
-        """Prevents profile fields when creating new user"""
-        if not obj:
+        # Prevents profile fields when creating new user or if user is staff
+        if not obj or obj.is_staff:
             return list()
         return super(UserAdmin, self).get_inline_instances(request, obj)
+
+    def get_readonly_fields(self, request, obj=None):
+        # Prevent staff changing their own permissions
+        rof = super(UserAdmin, self).get_readonly_fields(request, obj)
+        if not request.user.is_superuser:
+            rof += ('is_staff', 'is_superuser', 'groups', 'user_permissions')
+        return rof
+
+    def has_change_permission(self, request, obj=None):
+        # Prevent staff changing other user's who may have higher privileges.
+        has = super(UserAdmin, self).has_change_permission(request, obj)
+        if obj and not request.user.is_superuser:
+            if obj != request.user:
+                if obj.is_superuser or obj.user_permissions.exists():
+                    has = False
+        return has
 
 # Re-register user admin
 admin.site.unregister(User)
