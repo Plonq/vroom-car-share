0ad45c2526388e9277de4cb3f46c713e13e41486
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-20 11:48:47 +1000

    Overrode the 'logged out' template with our own

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 
 urlpatterns = [
+    # Override django auth templates with our own
+    url(r'^logout/$', LogoutView.as_view(template_name='accounts/logged_out.html')),
+    # Fall back on defaults for everything else
     url(r'^', include('django.contrib.auth.urls')),
+    # Other accounts-related pages not included in django auth
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
