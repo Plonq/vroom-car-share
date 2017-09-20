c9d449a99f7627d5a92cc258a43f445cca619df2
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-20 13:05:32 +1000

    Fix registration form

--- registration/views.py
+++ registration/views.py
@@ -1,9 +1,8 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserProfileForm, AddressForm, CreditCardForm
+from .forms import UserCreationForm, UserProfileForm, AddressForm, CreditCardForm
 
 
 def register(request):
@@ -13,27 +12,29 @@ def register(request):
 
     elif request.method == 'POST':
         user_form = UserCreationForm(request.POST)
-        if user_form.is_valid():
+        user_profile_form = UserProfileForm(request.POST)
+        address_form = AddressForm(request.POST)
+        credit_card_form = CreditCardForm(request.POST)
+
+        # Validate forms and save
+        if user_form.is_valid() and user_profile_form.is_valid() and address_form.is_valid() and credit_card_form.is_valid():
             user = user_form.save()
 
+            # Recreate profile with instance auto-created when user saved
             user_profile_form = UserProfileForm(request.POST, instance=user.profile)
-            address_form = AddressForm(request.POST)
-            credit_card_form = CreditCardForm(request.POST)
 
-            if user_profile_form.is_valid() and address_form.is_valid() and credit_card_form.is_valid():
-                user_profile_form.save()
+            user_profile_form.save()
+            address = address_form.save(commit=False)
+            address.user = user
+            address.save()
+            credit_card = credit_card_form.save(commit=False)
+            credit_card.user = user
+            credit_card.save()
 
-                address = address_form.save(commit=False)
-                address.user = user
-                address.save()
-                credit_card = credit_card_form.save(commit=False)
-                credit_card.user = user
-                credit_card.save()
+            # Auto-login as new user
+            login(request, user)
 
-                # TODO: Login the user
-
-                # Redirect to TODO: change to profile
-                return redirect('index')
+            return redirect('profile')
 
     else:
         user_form = UserCreationForm()
@@ -41,13 +42,13 @@ def register(request):
         address_form = AddressForm()
         credit_card_form = CreditCardForm()
 
-        context = {
-            'user_form': user_form,
-            'user_profile_form': user_profile_form,
-            'address_form': address_form,
-            'credit_card_form': credit_card_form
-        }
-        return render(request, 'registration/register.html', context)
+    context = {
+        'user_form': user_form,
+        'user_profile_form': user_profile_form,
+        'address_form': address_form,
+        'credit_card_form': credit_card_form
+    }
+    return render(request, 'registration/register.html', context)
 
 
 @login_required
