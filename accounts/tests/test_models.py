3473b7aefc05777895316c5f7b07f824e73e86ea
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-22 11:40:03 +1000

    Add basic model tests

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,3 +1,34 @@
+from django.core import mail
 from django.test import TestCase
 
+from ..models import *
 
+
+user = User(email='test@test.com', first_name='John', last_name='Doe', date_of_birth='2017-01-01')
+address = Address(address_line_1='Address Line 1', address_line_2='Address Line 2', city='City', state='VIC', postcode='3000')
+credit_card = CreditCard(card_number='1111222233334444')
+
+
+class AccountsUserModelTests(TestCase):
+    def test_full_name(self):
+        self.assertEqual(user.get_full_name(), 'John Doe')
+
+    def test_short_name(self):
+        self.assertEqual(user.get_short_name(), 'John')
+
+    def test_email_user(self):
+        user.email_user('Subject', 'Message', 'testrunner@test.com')
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Subject')
+        self.assertEqual(mail.outbox[0].body, 'Message')
+        self.assertEqual(mail.outbox[0].from_email, 'testrunner@test.com')
+
+
+class AccountsAddressModelTests(TestCase):
+    def test_string_output(self):
+        self.assertEqual(address.__str__(), 'Address Line 1, City, VIC 3000')
+
+
+class AccountsCreditCardModelTests(TestCase):
+    def test_string_output(self):
+        self.assertEqual(credit_card.__str__(), 'XXXX XXXX XXXX 4444')
