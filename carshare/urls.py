4919c7a190a38d48b10079d7e1c1d3d0c9b4e886
Steven Thompson <sthompson02@gmail.com>
2017-09-26 11:36:56 +1000

    Added findacar page with a basic map

--- carshare/urls.py
+++ carshare/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'contact_us/$', views.contact_us, name='contact_us'),
+    url(r'findacar/$', views.findacar, name='findacar'),
 ]
