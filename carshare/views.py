36005369f669f493d57c6ad7d001835fc7f7fb13
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-27 14:15:27 +1000

    Provide vehicles to Find A Car template

--- carshare/views.py
+++ carshare/views.py
@@ -1,7 +1,10 @@
 from django.core.mail import EmailMessage, BadHeaderError
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from .forms import ContactForm
+from .models import Vehicle
+
 
 # Create your views here.
 def index(request):
@@ -32,4 +35,8 @@ def contact_us(request):
     return render(request, "carshare/contact_us.html", {'contact_form': contact_form})
 
 def findacar(request):
-    return render(request, "carshare/findacar.html")
+    active_vehicles_with_pods = Vehicle.objects.filter(active=True).exclude(pod__isnull=True)
+    context = {
+        'vehicles': active_vehicles_with_pods
+    }
+    return render(request, "carshare/findacar.html", context)
