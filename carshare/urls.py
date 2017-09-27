d2f5d97a14ca1191e6e29df42ee3e8b29518bd4e
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-27 16:51:49 +1000

    Replace underscores with hyphens in URLs
    Better practice for SEO due to Google not treating underscores as word
    separators.

--- carshare/urls.py
+++ carshare/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'contact_us/$', views.contact_us, name='contact_us'),
+    url(r'contact-us/$', views.contact_us, name='contact_us'),
     url(r'findacar/$', views.findacar, name='findacar'),
 ]
