7df1648423c10ace8f3696de6a1b8342ed0039be
Huon Imberger <huon28@gmail.com>
2017-09-28 18:57:12 +1000

    Improving date and time fields (WIP)

--- carshare/forms.py
+++ carshare/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Button, Submit, Field
+from crispy_forms.bootstrap import FormActions
 from datetimewidget.widgets import DateTimeWidget
 
 from .models import Booking
@@ -18,36 +20,65 @@ class ContactForm(forms.Form):
         self.helper.form_tag = False
 
 
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = ["schedule_start", "schedule_end"]
+class BookingForm(forms.Form):
+    booking_start_date = forms.DateField()
+    booking_start_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    booking_end_date = forms.DateField()
+    booking_end_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
 
-        dateTimeOptions = {
-            'format': 'yyyy-mm-dd hh:00',
-            'minView': 1,
-            'minuteStep': 60,
-            'startDate': timezone.now().date().isoformat(),
-        }
-        widgets = {
-            'schedule_start': DateTimeWidget(bootstrap_version=3, options=dateTimeOptions),
-            'schedule_end': DateTimeWidget(bootstrap_version=3, options=dateTimeOptions),
-        }
+        # dateTimeOptions = {
+        #     'format': 'yyyy-mm-dd hh:00',
+        #     'minView': 1,
+        #     'minuteStep': 60,
+        #     'startDate': timezone.now().date().isoformat(),
+        # }
+        # widgets = {
+        #     'booking_start_date': ,
+        #     'booking_start_time': ,
+        # }
 
-    def clean(self):
-        cleaned_data = super(BookingForm, self).clean()
-        schedule_start = cleaned_data.get('schedule_start')
-        schedule_end = cleaned_data.get('schedule_end')
-        if schedule_start and schedule_end:
-            # Make sure schedule_end is later than schedule_start
-            if schedule_end < schedule_start:
-                raise forms.ValidationError('End time must be after the start time')
-            # Make sure schedule_start is in the future
-            if schedule_start <= timezone.now():
-                raise forms.ValidationError('Start time must be in the future')
-        return cleaned_data
+    # def clean(self):
+    #     cleaned_data = super(BookingForm, self).clean()
+    #     schedule_start = cleaned_data.get('schedule_start')
+    #     schedule_end = cleaned_data.get('schedule_end')
+    #     if schedule_start and schedule_end:
+    #         # Make sure schedule_end is later than schedule_start
+    #         if schedule_end < schedule_start:
+    #             raise forms.ValidationError('End time must be after the start time')
+    #         # Make sure schedule_start is in the future
+    #         if schedule_start <= timezone.now():
+    #             raise forms.ValidationError('Start time must be in the future')
+    #         # TODO Prevent time overlapping with existing booking
+    #     return cleaned_data
 
     def __init__(self, *args, **kwargs):
         super(BookingForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_tag = False
+        self.helper.form_class = 'validated-form'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Field('booking_start_date', css_class='datepicker'),
+                    css_class='col-sm-5',
+                ),
+                Div(
+                    'booking_start_time',
+                    css_class='col-sm-5',
+                ),
+                css_class='row',
+            ),
+            Div(
+                Div(
+                    Field('booking_end_date', css_class='datepicker'),
+                    css_class='col-sm-5',
+                ),
+                Div(
+                    'booking_end_time',
+                    css_class='col-sm-5',
+                ),
+                css_class='row',
+            ),
+            FormActions(
+                Submit('submit', 'Book', css_class='btn btn-primary'),
+            )
+        )
