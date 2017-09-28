890d5b6d85ca85a75098cb30bd873682c9ee4340
Huon Imberger <s3527782@student.rmit.edu.au>
2017-09-28 14:57:36 +1000

    Backend logic for creating a booking
    Front-end incomplete

--- carshare/forms.py
+++ carshare/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+
 from crispy_forms.helper import FormHelper
+from datetimewidget.widgets import DateTimeWidget, DateWidget
+
+from .models import Booking
+from django.utils import timezone
+
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(label='Your name', max_length=60)
@@ -8,5 +14,39 @@ class ContactForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ["schedule_start", "schedule_end"]
+
+        dateTimeOptions = {
+            'format': 'yyyy-mm-dd hh:00',
+            'minView': 1,
+            'minuteStep': 60,
+        }
+        widgets = {
+            'schedule_start': DateTimeWidget(bootstrap_version=3, options=dateTimeOptions),
+            'schedule_end': DateTimeWidget(bootstrap_version=3, options=dateTimeOptions),
+        }
+
+    def clean(self):
+        cleaned_data = super(BookingForm, self).clean()
+        schedule_start = cleaned_data.get('schedule_start')
+        schedule_end = cleaned_data.get('schedule_end')
+        if schedule_start and schedule_end:
+            # Make sure schedule_end is later than schedule_start
+            if schedule_end < schedule_start:
+                raise forms.ValidationError('End time must be after the start time')
+            # Make sure schedule_start is in the future
+            if schedule_start <= timezone.now():
+                raise forms.ValidationError('Start time must be in the future')
+        return cleaned_data
+
+    def __init__(self, *args, **kwargs):
+        super(BookingForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_tag = False
--- carshare/views.py
+++ carshare/views.py
@@ -1,9 +1,11 @@
+from django.contrib import messages
 from django.core.mail import EmailMessage, BadHeaderError
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-from .forms import ContactForm
-from .models import Vehicle
+from .forms import ContactForm, BookingForm
+from .models import Vehicle, Booking
 
 
 # Create your views here.
@@ -30,8 +32,9 @@ def contact_us(request):
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return render(request, "carshare/contact_us_success.html")
+    else:
+        contact_form = ContactForm()
 
-    contact_form = ContactForm()
     return render(request, "carshare/contact_us.html", {'contact_form': contact_form})
 
 def findacar(request):
@@ -40,3 +43,39 @@ def findacar(request):
         'vehicles': active_vehicles_with_pods
     }
     return render(request, "carshare/findacar.html", context)
+
+@login_required
+def new_booking(request, vehicle_name):
+    vehicle = Vehicle.objects.get(name__iexact=vehicle_name)
+
+    if request.method == 'POST':
+        booking_form = BookingForm(request.POST)
+        if booking_form.is_valid():
+            # Process form and create booking
+            booking = booking_form.save(commit=False)
+            print(booking.schedule_start)
+            booking.user = request.user
+            booking.vehicle = vehicle
+            booking.save()
+
+            messages.success(request, 'Booking created successfully')
+            return redirect('booking_detail', booking.pk)
+    else:
+        booking_form = BookingForm()
+
+    context = {
+        'vehicle': vehicle,
+        'booking_form': booking_form,
+    }
+    return render(request, "carshare/bookings/new.html", context)
+
+@login_required
+def booking_detail(request, booking_id):
+    booking = Booking.objects.get(pk=booking_id)
+    if request.user != booking.user:
+        messages.error(request, 'You do not have permission to view that booking')
+        return redirect('index')
+    context = {
+        'booking': booking,
+    }
+    return render(request, "carshare/bookings/detail.html", context)
