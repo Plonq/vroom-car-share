82a53981ba0a8e0e64e86df8950975b72ae17a32
Huon Imberger <s3527782@student.rmit.edu.au>
2017-10-03 11:08:59 +1100

    Add bookings to django-admin

--- carshare/admin.py
+++ carshare/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Vehicle, Pod, VehicleType
+from .models import Vehicle, Pod, VehicleType, Booking
 
 
 def make_active(modeladmin, request, queryset):
@@ -11,19 +11,28 @@ def make_inactive(modeladmin, request, queryset):
     queryset.update(active=False)
 make_inactive.short_description = "Mark selected vehicles as inactive"
 
+
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ['name', 'make', 'model', 'pod', 'active']
     ordering = ['name']
     actions = [make_active, make_inactive]
 
+
 class VehicleTypeAdmin(admin.ModelAdmin):
     list_display = ['description', 'hourly_rate', 'daily_rate']
     ordering = ['description']
+
 
 class PodAdmin(admin.ModelAdmin):
     list_display = ['description', 'vehicle']
     ordering = ['description']
 
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'vehicle', 'schedule_start', 'schedule_end']
+    ordering = ['schedule_start']
+
 admin.site.register(VehicleType, VehicleTypeAdmin)
 admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(Pod, PodAdmin)
+admin.site.register(Booking, BookingAdmin)
