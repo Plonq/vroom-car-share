21a12b2f2c7a70f3316e268751e76e006913e5f6
Huon Imberger <s3527782@student.rmit.edu.au>
2017-10-10 10:38:00 +1100

    Restructure navigation

--- carshare/urls.py
+++ carshare/urls.py
@@ -6,10 +6,9 @@ from . import views
 app_name = 'carshare'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'my-account/$', views.my_account, name='my_account'),
     url(r'contact-us/$', views.contact_us, name='contact_us'),
     url(r'find-a-car/$', views.find_a_car, name='find_a_car'),
     url(r'bookings/new/(?P<vehicle_id>[0-9]+)/$', views.booking_create, name='booking_create'),
     url(r'bookings/(?P<booking_id>[0-9]+)/$', views.booking_detail, name='booking_detail'),
-    url(r'bookings/$', views.booking_index, name='booking_history'),
+    url(r'bookings/$', views.booking_index, name='booking_index'),
 ]
