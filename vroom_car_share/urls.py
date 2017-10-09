cf3a0b4e4974379570631a723a21aae12305423f
Huon Imberger <s3527782@student.rmit.edu.au>
2017-10-09 15:53:05 +1100

    Configure title and header for django admin

--- vroom_car_share/urls.py
+++ vroom_car_share/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+# Site details (affects admin)
+admin.site.site_title = 'Vroom Admin'
+admin.site.site_header = 'Vroom Car Share Admin'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
