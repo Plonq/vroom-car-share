61f7ee73ab991241cc299af4f0525dd969a7887a
Huon Imberger <s3527782@student.rmit.edu.au>
2017-10-27 09:30:23 +1100

    Remove unused import

--- carshare/views.py
+++ carshare/views.py
@@ -12,7 +12,6 @@ import json
 
 from .forms import ContactForm, BookingForm, ExtendBookingForm
 from .models import Vehicle, Booking, Invoice, Pod
-from accounts.models import User
 
 
 # Create your views here.
