014f1eac6171591f11570747dfa40512870d6286
Shaun <shaunomalley1988@hotmail.com>
2017-11-02 14:49:50 +0800

    trialling datetime session passing

--- carshare/urls.py
+++ carshare/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+
 app_name = 'carshare'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'testering/$', views.trial_test, name='trial_test'),
     url(r'contact-us/$', views.contact_us, name='contact_us'),
     url(r'pricing/$', views.pricing, name='pricing'),
     url(r'how-it-works/$', views.how_it_works, name='how_it_works'),
